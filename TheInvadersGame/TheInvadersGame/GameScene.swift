093c25e0dd9c58d77eca3bac86cddec6157657c2
Igor de Castro <igor.cbs@gmail.com>
2020-06-30 11:20:08 -0300

    Commit inicial

---
+++ TheInvadersGame/TheInvadersGame/GameScene.swift
@@ -0,0 +1,55 @@
+//
+//  GameScene.swift
+//  TheInvadersGame
+//
+//  Created by Igor de Castro on 30/06/20.
+//  Copyright © 2020 Igor de Castro. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+        
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+       
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
